ff4c837e0e39970a4fd1eafec61660fbf1fce03a
Filipe Alves <filipe.alvesrr@hotmail.com>
2023-07-27 17:23:38 -0300

    Último ponto para início com refatoração com herança

--- src/server.js
+++ src/server.js
@@ -4,9 +4,32 @@ const descartEco = express()
 // ESTILOS
 descartEco.use(express.static("public"))
 
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: descartEco,
+    noCache: true
+})
+
 // ROTAS
 descartEco.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+   return res.render("index.html")
 })
+
+descartEco.get("/register", (req, res) => {
+    return res.render("register.html")
+})
+
+descartEco.get("/points", (req, res) => {
+    return res.render("collectionPoints.html")
+})
+
+descartEco.get("/about-us", (req, res) => {
+    return res.render("aboutUs.html")
+})
+
+descartEco.get("/know-more", (req, res) => {
+    return res.render("knowMore.html")
+})
+
 
 descartEco.listen(2000)
