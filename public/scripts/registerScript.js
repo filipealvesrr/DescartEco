2970d8ec06c697236db7e62e21e14a86a6ecbed5
Filipe Alves <filipe.alvesrr@hotmail.com>
2023-07-30 16:45:02 -0300

    Alterações de firebase, refatoração de código e afins

--- public/scripts/registerScript.js
+++ public/scripts/registerScript.js
@@ -1,24 +1,43 @@
-const modalOverlay = document.querySelector('.modal-overlay');
-const buttonLink = document.getElementById('submitBtn');
+// const modalOverlay = document.querySelector('.modal-overlay');
+// const buttonLink = document.getElementById('submitBtn');
 
-buttonLink.addEventListener('click', function(event) {
-    event.preventDefault(); // Evitar o envio padrão do formulário
-    modalOverlay.classList.add('active');
+// buttonLink.addEventListener('click', function(event) {
+//     event.preventDefault(); // Evitar o envio padrão do formulário
+//     modalOverlay.classList.add('active');
 
-    Swal.fire({
-        title: 'Cadastro realizado!',
-        text: 'Pesquise seu ponto de coleta em Home',
-        icon: 'success',
-        confirmButtonText: 'Ok',
-        customClass: {
-            popup: 'sweet-style',
-        }
-    }).then((result) => {
-        if (result.isConfirmed) {
-            // Confirmação recebida, enviar o formulário
-            document.getElementById("myForm").submit();
-        } else {
-            document.getElementById("myForm").submit();
-        }
-    });
+//     Swal.fire({
+//         title: 'Cadastro realizado!',
+//         text: 'Pesquise seu ponto de coleta em Home',
+//         icon: 'success',
+//         confirmButtonText: 'Ok',
+//         customClass: {
+//             popup: 'sweet-style',
+//         }
+//     }).then((result) => {
+//         if (result.isConfirmed) {
+//             // Confirmação recebida, enviar o formulário
+//             document.getElementById("myForm").submit();
+//         } else {
+//             document.getElementById("myForm").submit();
+//         }
+//     });
+// });
+
+// Configuração do Whatsapp
+
+const telefoneInput = document.getElementById('whats');
+
+telefoneInput.addEventListener('input', function(event) {
+    const input = event.target;
+    const inputValue = input.value.replace(/\D/g, ''); // Remove todos os caracteres não numéricos
+    const formattedValue = formatarTelefone(inputValue);
+    input.value = formattedValue;
 });
+
+function formatarTelefone(telefone) {
+    if (telefone.length > 11) {
+        telefone = telefone.slice(0, 11);
+      }
+    const padraoTelefone = /^(\d{2})(\d{5})(\d{4})$/;
+    return telefone.replace(padraoTelefone, '($1) $2-$3');
+}
--- src/server.js
+++ src/server.js
@@ -1,35 +1,88 @@
-const express = require("express")
-const descartEco = express()
+const express = require("express");
+const descartEco = express();
 
+// CONFIGURAÇÕES DO FIREBASE
+
+const admin = require("firebase-admin");
+const serviceAccount = require("./database/descarteco-48033-firebase-adminsdk-mvske-07aebc618c.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+
+
+// RECEBER REQ.BODY
+descartEco.use(express.urlencoded({ extended: true }));
 // ESTILOS
-descartEco.use(express.static("public"))
+descartEco.use(express.static("public"));
 
 const nunjucks = require("nunjucks")
 nunjucks.configure("src/views", {
     express: descartEco,
     noCache: true
-})
+});
 
 // ROTAS
 descartEco.get("/", (req, res) => {
-   return res.render("index.html")
-})
+   return res.render("index.html");
+});
 
 descartEco.get("/register", (req, res) => {
-    return res.render("register.html")
-})
+    return res.render("register.html", { success: true});
+});
+
+descartEco.post("/success", (req, res) => {
+    const newPoint = db.collection('places')
+    let { name, address, complement, city, email, whats, itens } = req.body;
+    
+    if(Array.isArray(itens)) {
+        itens = itens.join(", ")
+    }
+
+    const pointData = {
+        name,
+        address,
+        complement,
+        city,
+        email, 
+        whats,
+        itens
+    }
+    
+    newPoint.add(pointData).then((doc) => {
+        console.log('Documento adicionado com ID:', doc.id);
+        return res.render("/success", { success: true})
+    }).catch((err) => {
+        console.error('Erro ao adicionar documento:', err);
+    });
+
+
+});
 
 descartEco.get("/points", (req, res) => {
-    return res.render("collectionPoints.html")
-})
+    const city = req.query.city;
+    const pointsCollection = db.collection('places');
+
+    pointsCollection.where('city', '==', city).get().then((snapshot) => {
+        const points = [];
+        snapshot.forEach(doc => points.push(doc.data()));
+
+        return res.render("collectionPoints.html", {points, city});
+    }).catch((err) => {
+        console.error("Erro ao obter os dados de coleta:", err);
+        res.status(500).json({ err: 'Ocorreu um erro ao obter os pontos de coleta.' });
+    });
+});
 
 descartEco.get("/about-us", (req, res) => {
-    return res.render("aboutUs.html")
-})
+    return res.render("aboutUs.html");
+});
 
 descartEco.get("/know-more", (req, res) => {
-    return res.render("knowMore.html")
-})
+    return res.render("knowMore.html");
+});
 
 
-descartEco.listen(2000)
+descartEco.listen(4000);
